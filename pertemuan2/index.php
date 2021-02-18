f914e9a2b8ae4a8675bf388994de9e26a4fb5a6d
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2021-02-18 18:30:38 +0700

    menyelesaikan video peretemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Ryan fany fadlyllah Ar
+203040129
+https://github.com/RyanfanyfadlyllahAR/pw_2021_203040129.git
+pertemuan 2 - 15 febuari 2021
+mempelajari mengenai sintaks  PHP
+*/
+?>
+<?php
+// Standar Output
+// echo, print
+?>
+
