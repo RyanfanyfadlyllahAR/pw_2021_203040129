e2e47501025a207d9aadb9ac0d253d1d2a59637f
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2021-03-05 07:44:25 +0700

    mengerjakan pertemuan5

--- pertemuan5/latihan3.php
+++ pertemuan5/latihan3.php
@@ -1,20 +1,29 @@
 <?php 
-$mahasiswa = ["Ryan fany fadlyllah AR", "203040129", "Teknik informatika", "fadlyllahfanny@gmail.com"];
+$mahasiswa = [
+	["Ryan fany fadlyllah AR", "203040129", "Teknik
+	 informatika", "fadlyllahfanny@gmail.com"],
+	 ["Bagja solehudin", "203060013", "Teknik
+	 perencanaan wilayah kota", "baso@gmail.com"],
+	 ["Rosa yuni cahyani", "2030405060", "Teknologi
+	  pangan", "rosa@gmail.com"]
+];
+
  ?>
  <!DOCTYPE html>
  <html>
  <head>
- 	<title>Daftar mahasiswa</title>
+ 	<title>Daftar Mahasiswa</title>
  </head>
  <body>
 
  	<h1>Daftar Mahasiswa</h1>
-
+<?php  foreach($mahasiswa as $mhs):?>
  	<ul>
- 		<?php foreach( $mahasiswa as $mhs) : ?>
- 			<li><?= $mhs; ?></li>
- 		<?php endforeach; ?>
+	 	<li>Nama : <?= $mhs[0]; ?></li>
+	 	<li>Npm : <?= $mhs[1]; ?></li>
+	 	<li>jurusan : <?= $mhs[2]; ?></li>
+	 	<li>Email : <?= $mhs[3]; ?></li>
  	</ul>
- 
+ <?php endforeach; ?>
  </body>
  </html>
