39d82d19158b10cb0c2e6fc889b8cd0d45c9c398
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2021-05-02 15:30:02 +0700

    menyelesaikan pertemuan10

---
+++ pertemuan10/latihan3.php
@@ -0,0 +1,45 @@
+<?php 
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, intial-scale=1.0">
+	<title>Daftar Mahasiswa</title>
+</head>
+
+
+<body>
+	<h3>Daftar Mahasiswa</h3>
+	<a href="tambah.php">Tambah data mahasiswa</a>
+	<br><br>
+
+	<table border="1" cellpadding="10" cellspacing="0">
+		<tr>
+			<th>#</th>
+			<th>Gambar</th>
+			<th>NRP</th>
+			<th>Aksi</th>
+		</tr>
+
+		<?php $i = 1;
+		foreach($mahasiswa as $mhs) : ?>
+		<tr>
+			<td><?= $i++; ?></td>
+			<td><img src="img/<?= $mhs['img']; ?>" width="60"></td>
+			<td><?= $mhs['nama']; ?></td>
+			<td>
+				<a href="detail.php?id=<?= $mhs['id'];?>">lihat detail</a>
+			</td>
+		</tr>
+		<?php endforeach; ?>
+	</table>
+</body>
+
+</html>
+
+
